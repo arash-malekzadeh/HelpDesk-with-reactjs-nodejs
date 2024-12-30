26bb4f422049342ae727205279c2f8981532abaf
arash-malekzadeh <arash.malekzade@gmail.com>
2024-12-30 18:30:25 +0330

    Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,30 @@
+import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
+import { Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom";
+import Tasks from "./pages/Tasks";
+import Users from "./pages/Users";
+import TaskDetails from "./pages/TaskDetails";
+import { Toaster } from "sonner";
 
 function App() {
   return (
-    <>
-      <Login />
-    </>
+    <main className="w-full min-h-screen bg-[#f3f4f6]">
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/tasks" element={<Tasks />} />
+          <Route path="/completed/:status" element={<Tasks />} />
+          <Route path="/in-progress/:status" element={<Tasks />} />
+          <Route path="/todo/:status" element={<Tasks />} />
+          <Route path="/team" element={<Users />} />
+          <Route path="/trashed" element={<Trash />} />
+          <Route path="/task/:id" element={<TaskDetails />} />
+        </Route>
+        <Route path="/log-in" element={<Login />} />
+      </Routes>
+      <Toaster richColors />
+    </main>
   );
 }
 
